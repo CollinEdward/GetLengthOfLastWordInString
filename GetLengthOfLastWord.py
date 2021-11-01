c87a5aaa813531b73f0ad535a633c2156609f315
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2021-11-01 18:40:01 +0000

    Update GetLengthOfLastWord.py

--- GetLengthOfLastWord.py
+++ GetLengthOfLastWord.py
@@ -1,18 +1,3 @@
-class Solution(object):
-    def lengthOfLastWord(s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        
-            # When we don't tell it where to split it will just split everywhere that isn't a string    
-        lis = list(s.split())
-        return len(lis[-1])
-    
-    s = "  - fly me ..  to ,  the moon    "
-    print(lengthOfLastWord(s))
-
-    # You can do this is you aren't using return I think another example is
 
 class Solution(object):
     def lengthOfLastWord(s):
@@ -23,6 +8,3 @@ class Solution(object):
         
         lis = list(s.split())
         return len(lis[-1])
-
-    # This will do the same, but you aren't printing, so this is faster and shorter
-
